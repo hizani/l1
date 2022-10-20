d71d4d6351bac3bd5c270ec0995ae725b2086f1f
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-10-20 19:11:09 +0300

    Add comments to 2/op2

--- 2/op2/main.go
+++ 2/op2/main.go
@@ -9,18 +9,26 @@ import (
 )
 
 func main() {
+	// Создаем массив
 	base := [5]int32{2, 4, 6, 8, 10}
 	out := [5]int32{}
 
+	// Инициализируем wg
 	var wg sync.WaitGroup
 	wg.Add(len(base))
 
+	// Проходимся по массиву
 	for idx, num := range base {
+		// Передаем индекс и элемент по значению в горутину
 		go func(idx int, num int32) {
+			// Сохраняем результат в новый массив
 			out[idx] = num * num
+			// уменьшаем счетчик wg
 			wg.Done()
 		}(idx, num)
 	}
+	// Ждем завершения всех горутин
 	wg.Wait()
+	// Выводим массив
 	fmt.Println(out)
 }
