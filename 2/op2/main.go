0d2093feff22e4c114c943a9ffeba125611cda14
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-10-20 18:03:51 +0300

    Add 2

---
+++ 2/op2/main.go
@@ -0,0 +1,30 @@
+// Написать программу, которая конкурентно рассчитает значение квадратов
+// чисел взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	base := [5]int32{2, 4, 6, 8, 10}
+	out := [5]int32{}
+
+	var wg sync.WaitGroup
+	wg.Add(len(base))
+
+	for idx, num := range base {
+		go func(idx int, num int32) {
+			out[idx] = sqr(num)
+			wg.Done()
+		}(idx, num)
+	}
+	wg.Wait()
+	fmt.Println(out)
+}
+
+func sqr(num int32) int32 {
+	return num * num
+}
